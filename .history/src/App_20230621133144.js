545c01b4f4829ac2b6bca4d19cbc85b0a3dab300
Rakesh <rakeshkolpepatil@gmail.com>
2023-06-21 15:24:42 +0530

    first commit

--- .history/src/App_20230621133144.js
+++ .history/src/App_20230621133144.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="app" ></div>
+  return <div></div>
 }
 
 export default App
--- .history/src/App_20230621133157.js
+++ .history/src/App_20230621133157.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="app" ></div>
+  return <div className="App" ></div>
 }
 
 export default App
